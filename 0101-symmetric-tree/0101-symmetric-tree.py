f80a91fd840da87a06d99530ea1acfd5bf3fb763
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-03-13 15:49:35 +0530

    Time: 38 ms (49.65%), Space: 13.9 MB (90.34%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -4,6 +4,9 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# Follow Up 
+# 1. Recursive
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         if not root:
@@ -18,3 +21,27 @@ class Solution:
         if left.val != right.val:
             return False
         return self.helper(left.left, right.right) and self.helper(left.right, right.left)
+
+# 2.Iterative
+# def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+#         if root == None:
+#             return True
+#         stack1 = []
+#         stack2 = []
+#         stack1.append(root.left)
+#         stack2.append(root.right)
+        
+#         while stack1 and stack2:
+#             p = stack1.pop()
+#             q = stack2.pop()
+#             if (p==None and q!=None) or (q==None and p!=None) :
+#                 return False
+#             if p:
+#                 if p.val != q.val:
+#                     return False
+#                 stack1.append(p.left)
+#                 stack1.append(p.right)
+#                 stack2.append(q.right)
+#                 stack2.append(q.left)
+                
+#         return True
