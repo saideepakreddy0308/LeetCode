0164048a68bbf0a8048bc46822f6ff6972875c80
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-03-14 21:52:48 +0530

    Time: 32 ms (70.48%), Space: 13.8 MB (54.57%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -4,29 +4,15 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-# Using DFS Approach
-# 1.we want sum all routes from root and return sum.
-# 2.for this appropriate method would be dfs.
-# 3.using dfs traverse till end of tree ie. leaf node.
-# 4.keep track of path from root till leaf-node, now when encountered leaf node, sum it in answer.
-# 5.do this till all path covered, return answer.
-
-# A non-local variable is a variable used when there are nested functions, and we don’t want the variable of the inside function to be local within that function.
-
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        ans = 0
-        def dfs(curr=None,path=''):
-            nonlocal ans
-            if curr:
-                path += str(curr.val)
-                dfs(curr.left,path)
-                dfs(curr.right,path)
-                if curr.left == None and curr.right == None:ans += int(path)
-            return
-        dfs(root)
-        return ans        
-
-# Time complexity: O(N)
-# Space complexity: O(N)
+        def dfs(node,s):
+            if not node.left and not node.right:
+                return int(s)
+            total = 0
+            if node.left:
+                 total += dfs(node.left,s+ str(node.left.val))
+            if node.right:
+                total += dfs(node.right,s+ str(node.right.val))
+            return total
+        return dfs(root,str(root.val))
