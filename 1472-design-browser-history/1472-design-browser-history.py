1d4f8657951366a55311f86998716a1555b0b842
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-03-18 12:02:24 +0530

    Time: 214 ms (91.17%), Space: 16.8 MB (10.57%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -1,31 +1,27 @@
-# Method - 1
-
-class ListNode:
-    def __init__(self,val,prev=None,next=None):
-        self.val = val
-        self.prev = prev
-        self.next = next
+# Method - 2
 
 class BrowserHistory:
 
     def __init__(self, homepage: str):
-        self.cur = ListNode(homepage)
+        self.i = 0  # Pointer
+        self.len = 1
+        self.history = [homepage]
 
     def visit(self, url: str) -> None:
-        self.cur.next = ListNode(url,self.cur)
-        self.cur = self.cur.next
+        if len(self.history) < self.i + 2:
+            self.history.append(url)
+        else:
+            self.history[self.i + 1] = url
+        self.i += 1
+        self.len = self.i + 1
 
     def back(self, steps: int) -> str:
-        while self.cur.prev and steps>0:
-            self.cur = self.cur.prev
-            steps -= 1
-        return self.cur.val
+        self.i = max(self.i - steps,0)
+        return self.history[self.i]
 
     def forward(self, steps: int) -> str:
-        while self.cur.next and steps>0:
-            self.cur = self.cur.next
-            steps -= 1
-        return self.cur.val
+        self.i = min(self.i + steps, self.len - 1)
+        return self.history[self.i]
 
 
 # Your BrowserHistory object will be instantiated and called as such:
