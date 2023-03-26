ac2b941367aef0611e39b6e141b3ee29d698bc24
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-03-26 22:00:09 +0530

    Time: 1580 ms (32.40%), Space: 186.9 MB (10.28%) - LeetHub

---
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
@@ -0,0 +1,27 @@
+class Solution:
+    def longestCycle(self, edges: List[int]) -> int:
+        N = len(edges)
+        
+        def dfs(node, step):
+            if node in total:
+                return total[node]
+            if node in visited:
+                return step-visited[node]
+            else:
+                visited[node] = step
+                next_node = edges[node]
+                if next_node == -1:
+                    return -1
+                return dfs(next_node, step+1)
+                
+        
+        total = defaultdict(int)
+        for e in range(len(edges)):
+            if e not in total:
+                visited = defaultdict(int)
+                res = dfs(e, 0)
+                for v in visited:
+                    total[v] = res
+                    
+        ans = max(total.values())
+        return -1 if ans == 0 else ans 
