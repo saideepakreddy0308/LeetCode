2996130cb5155b48d1e0954130312907c885ce3f
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-04-01 14:49:53 +0530

    Time: 248 ms (44.70%), Space: 15.5 MB (58.56%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,22 +1,10 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        # Find the insertion position `idx`.
+        # This method uses upper_bound or bisect.bisect_right
+        idx = bisect.bisect_right(nums, target)
 
-        # Looking for target in the array, we look into insert position where we can put target, in without disrupting order
-        # Concept:  Find Lower Bound, here we look for leftmost insert position        
-        left = 0
-        right = len(nums)
-        
-        while left < right:
-            mid = (left + right) // 2
-            if nums[mid] >= target:
-                right = mid
-            else:
-                left = mid + 1
-        
-        if left < len(nums) and nums[left] == target:
-            return left
+        if idx > 0 and nums[idx - 1] == target:
+            return idx - 1
         else:
             return -1
-        
-    # Time Complexity: O(logn)
-    # Space Complexity: O(1)
