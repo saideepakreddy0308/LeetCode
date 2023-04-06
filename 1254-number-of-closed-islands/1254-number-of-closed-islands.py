bd34b2cfa7f8b135f416462ffc95db93ca64f984
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-04-06 13:01:39 +0530

    Time: 126 ms (76.14%), Space: 14.3 MB (93.84%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -1,28 +1,36 @@
+# Breadth First Search Approach
+
+from collections import deque
+
 class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
+        # Initialize variables
+        count = 0
+        m, n = len(grid), len(grid[0])
+        q = deque()
+        visited = [[False for _ in range(n)] for _ in range(m)]
         
-        ROWS, COLS = len(grid), len(grid[0])
-        visit = set()
-        
-        def dfs(r,c):
-            
-            # If its open island
-            if (r < 0 or c < 0 or r == ROWS or c == COLS):
-                return 0  # False
-            # If water, we need not to run DFS and also if visited
-            if grid[r][c] == 1 or (r,c) in visit:  
-                return 1 # True, this is a closed island, as far as concerned
-            # Hashset, mark the land tile visited, to avoid infinite recursions
-            visit.add((r,c))
-            
-            return min(dfs(r+1,c), dfs(r-1,c), dfs(r,c+1), dfs(r,c-1))
-        result = 0
-        for r in range(ROWS):
-            for c in range(COLS):
-                # if its land and not visited, we dont want to do recursion on it again.
-                if not grid[r][c] and (r,c) not in visit:
-                    result += dfs(r,c)
-        return result   
-
-# Time Complexity: O(M*N)
-# Space Complexity: O(M*N), worst case scenerio
+        # Check all cells in the grid
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 0 and not visited[i][j]:
+                    closed = True
+                    q.append((i, j))
+                    visited[i][j] = True
+                    
+                    # BFS to check if the island is closed
+                    while q:
+                        x, y = q.popleft()
+                        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                            nx, ny = x + dx, y + dy
+                            if nx < 0 or nx >= m or ny < 0 or ny >= n:
+                                closed = False
+                            elif grid[nx][ny] == 0 and not visited[nx][ny]:
+                                q.append((nx, ny))
+                                visited[nx][ny] = True
+                    
+                    # Increase count if the island is closed
+                    if closed:
+                        count += 1
+                        
+        return count
