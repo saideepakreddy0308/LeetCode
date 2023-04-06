feb2a568530b84f386e5427bc351ab5957a2c4df
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-04-06 11:36:59 +0530

    Time: 140 ms (32.75%), Space: 16 MB (5.36%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -1,44 +1,36 @@
 class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
+        
+        # Intuition:
+            # To go through all the cells and check whether its neighbours are matching the condition i.e top,right, bottom, left are surrounded by 1. If so then its a closed island else move on.
+            
+        # Approach
+            # 1. Firstly, loop through the rows and cols
+            # 2. Maintain a SET. Why? Let's say we are on the cell (say c) and we searched its neightbours (say n number) for 1 or 0. Now in the future iterations we will be eventually going to iterate over n (because of our loops). So this will lead to iterate over (c) even though we have already checked that cell and will lead to unnecessary recursive calls. To avoid this we will mark that cell (c) as visited by adding it to the set.
+            # 3. Create a DFS funtion which will only return 0 or 1. "O" if out of bounds and "1" if the cell = 1 or if we have visited it.
+            # 4. If either of the above condition fails then that means it is a completely new cell and we can carry on the recursion.
+            # 5.Finally return min of all 4 recursice calls. Why min? Because lets say in our first call row got out of bounds which means it will return 0 and we know if any row or col reaches its boundaries then we must return 0 so we just use min to calculate that.
 
-        # go through the grid and make a dfs per island
-        result = 0
-        m = len(grid)
-        n = len(grid[0])
-        for rx in range(m):
-            for cx in range(n):
 
-                # check whether we found an island
-                if grid[rx][cx] == 0 and self.islandDfs(rx, cx, grid, m, n):
-                    result += 1
-        return result
+        ROWS, COLS = len (grid), len(grid[0])
+        visit = set()
+        
+        def dfs(r, c):
+            if (r <0 or c <0 or
+                r == ROWS or c == COLS):
+                    return 0 # False
+            if grid[r][c] == 1 or (r, c) in visit:
+                return 1 # True
+            visit.add((r, c))
+
+            return min(dfs(r+1, c), dfs(r-1,c), dfs(r, c+1), dfs(r, c-1))
+        
+        res = 0
+        for r in range (ROWS):
+            for c in range (COLS):
+                if not grid[r][c] and (r, c) not in visit:
+                    res += dfs(r, c)
+        return res
     
-
-    def islandDfs(self, rx, cx, grid, m, n):
-        
-        # check whether we are out of bounds
-        if rx < 0 or cx < 0 or rx >= m or cx >=n:
-            return False
-        
-        # check whether we are water
-        if grid[rx][cx] == 1:
-            return True
-        
-        # check whether we already visited
-        if grid[rx][cx] == -1:
-            return True
-
-        # set ourself to visited
-        grid[rx][cx] = -1
-
-        # traverse in all directions
-        result = True
-        for rx, cx in [(rx-1, cx), (rx+1, cx), (rx, cx-1), (rx, cx+1)]:
-            if not self.islandDfs(rx, cx, grid, m, n):
-                result = False
-        
-        # check all sourrounding cells
-        return result
-
-# Here, T.C = O(M*N)
-# S.C = O(1), but if considered stack-recursion O(M*N)
+# Time Complexity: O(M*N)
+# Space Complexity: O(1)
