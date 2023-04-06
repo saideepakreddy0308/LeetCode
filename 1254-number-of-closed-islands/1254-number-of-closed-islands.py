8566b5f9dafdabcb2bb1166c000fb19d4af1207d
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-04-06 12:04:37 +0530

    Time: 143 ms (26.66%), Space: 16.1 MB (5.36%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -1,36 +1,28 @@
 class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
         
-        # Intuition:
-            # To go through all the cells and check whether its neighbours are matching the condition i.e top,right, bottom, left are surrounded by 1. If so then its a closed island else move on.
-            
-        # Approach
-            # 1. Firstly, loop through the rows and cols
-            # 2. Maintain a SET. Why? Let's say we are on the cell (say c) and we searched its neightbours (say n number) for 1 or 0. Now in the future iterations we will be eventually going to iterate over n (because of our loops). So this will lead to iterate over (c) even though we have already checked that cell and will lead to unnecessary recursive calls. To avoid this we will mark that cell (c) as visited by adding it to the set.
-            # 3. Create a DFS funtion which will only return 0 or 1. "O" if out of bounds and "1" if the cell = 1 or if we have visited it.
-            # 4. If either of the above condition fails then that means it is a completely new cell and we can carry on the recursion.
-            # 5.Finally return min of all 4 recursice calls. Why min? Because lets say in our first call row got out of bounds which means it will return 0 and we know if any row or col reaches its boundaries then we must return 0 so we just use min to calculate that.
-
-
-        ROWS, COLS = len (grid), len(grid[0])
+        ROWS, COLS = len(grid), len(grid[0])
         visit = set()
         
-        def dfs(r, c):
-            if (r <0 or c <0 or
-                r == ROWS or c == COLS):
-                    return 0 # False
-            if grid[r][c] == 1 or (r, c) in visit:
-                return 1 # True
-            visit.add((r, c))
+        def dfs(r,c):
+            
+            # If its open island
+            if (r < 0 or c < 0 or r == ROWS or c == COLS):
+                return 0  # False
+            # If water, we need not to run DFS and also if visited
+            if grid[r][c] == 1 or (r,c) in visit:  
+                return 1 # True, this is a closed island, as far as concerned
+            # Hashset, mark the land tile visited, to avoid infinite recursions
+            visit.add((r,c))
+            
+            return min(dfs(r+1,c), dfs(r-1,c), dfs(r,c+1), dfs(r,c-1))
+        result = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                # if its land and not visited, we dont want to do recursion on it again.
+                if not grid[r][c] and (r,c) not in visit:
+                    result += dfs(r,c)
+        return result   
 
-            return min(dfs(r+1, c), dfs(r-1,c), dfs(r, c+1), dfs(r, c-1))
-        
-        res = 0
-        for r in range (ROWS):
-            for c in range (COLS):
-                if not grid[r][c] and (r, c) not in visit:
-                    res += dfs(r, c)
-        return res
-    
 # Time Complexity: O(M*N)
-# Space Complexity: O(1)
+# Space Complexity: O(M*N), worst case scenerio
