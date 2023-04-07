90585d8e23a3585fea9eed6d8e86450b017a277a
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-04-07 12:05:43 +0530

    Time: 234 ms (91.64%), Space: 15.5 MB (57.18%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,22 +1,24 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # Approach - 2 ( Upper Bound)
-         # We look for target in the arrat and look into insert position without distrupting order
-            
-            l = 0
-            r = len(nums)
-            
-            while l < r:
-                mid = l + (r-l)//2
-                # Case_1:
-                if nums[mid] <= target:
-                    l = mid + 1
-                else:  # nums[mid] > target
-                    r = mid
-            if l > 0 and nums[l-1] == target:
-                return l - 1
+        
+        # APPROACH - 3 (lower bound)
+        # Looking for target in the array, and insert into the positon where we can put target without disrupting order
+        # Looking for left most insert position
+        
+        l = 0
+        r = len(nums)
+        
+        while l < r:
+            mid = l + (r-l)//2
+            if nums[mid] >= target:
+                r = mid
             else:
-                return -1 
-            
-        # Time Complexity: O(logN)
-        # Space Complexity: O(1)
+                l = mid + 1
+        
+        if l < len(nums) and nums[l] == target:
+            return l
+        else:
+            return -1
+        
+        # T.C: O(logN)
+        # S.C: O(1)
