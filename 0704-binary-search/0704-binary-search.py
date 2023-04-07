36c06245b1b341f69cd0ce2c9e5f497b82a32ec7
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-04-07 11:39:21 +0530

    Time: 245 ms (50.02%), Space: 15.5 MB (12.89%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,10 +1,23 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # Find the insertion position `idx`.
-        # This method uses upper_bound or bisect.bisect_right
-        idx = bisect.bisect_right(nums, target)
-
-        if idx > 0 and nums[idx - 1] == target:
-            return idx - 1
-        else:
-            return -1
+        
+        # APPROACH - 1
+        # Initialize left and Right Pointers
+        l = 0
+        r = len(nums) - 1
+        
+        # Binary Search
+        while l<=r:
+            mid = (l + (r-l)//2)
+            # Case_1:
+            if nums[mid] == target:
+                return mid
+            # Case_2:
+            elif nums[mid] > target:
+                r = mid - 1
+            # Case_3
+            else:
+                l = mid + 1
+        return -1
+    # T.C : O(logN)
+    # S.C : O(1)
