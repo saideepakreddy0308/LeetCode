5921e31c211d4f662e9794bca98fd9c08df63921
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-04-07 11:53:49 +0530

    Time: 243 ms (59.33%), Space: 15.4 MB (57.18%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,23 +1,22 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
-        # APPROACH - 1
-        # Initialize left and Right Pointers
-        l = 0
-        r = len(nums) - 1
-        
-        # Binary Search
-        while l<=r:
-            mid = (l + (r-l)//2)
-            # Case_1:
-            if nums[mid] == target:
-                return mid
-            # Case_2:
-            elif nums[mid] > target:
-                r = mid - 1
-            # Case_3
+        # Approach - 2 ( Upper Bound)
+         # We look for target in the arrat and look into insert position without distrupting order
+            
+            l = 0
+            r = len(nums)
+            
+            while l < r:
+                mid = l + (r-l)//2
+                # Case_1:
+                if nums[mid] <= target:
+                    l = mid + 1
+                else:  # nums[mid] > target
+                    r = mid
+            if l > 0 and nums[l-1] == target:
+                return l - 1
             else:
-                l = mid + 1
-        return -1
-    # T.C : O(logN)
-    # S.C : O(1)
+                return -1 
+            
+        # Time Complexity: O(logN)
+        # Space Complexity: O(1)
