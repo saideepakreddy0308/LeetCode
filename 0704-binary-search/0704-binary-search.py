bc4a00e8299a7b88d6685c65024cdca1ffb21afb
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-04-07 12:12:50 +0530

    Time: 233 ms (93.50%), Space: 15.4 MB (96.46%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,24 +1,18 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        # Finding Upper bound
+        idx = bisect.bisect_right(nums,target)
         
-        # APPROACH - 3 (lower bound)
-        # Looking for target in the array, and insert into the positon where we can put target without disrupting order
-        # Looking for left most insert position
-        
-        l = 0
-        r = len(nums)
-        
-        while l < r:
-            mid = l + (r-l)//2
-            if nums[mid] >= target:
-                r = mid
-            else:
-                l = mid + 1
-        
-        if l < len(nums) and nums[l] == target:
-            return l
+        if idx > 0 and nums[idx-1] == target:
+            return idx - 1
         else:
             return -1
         
-        # T.C: O(logN)
-        # S.C: O(1)
+        # T.C : O(logN)
+        # S.C:  O(1)
+        
+        # The bisect.bisect_right function returns the position in the array where the target value could be inserted without disrupting the sorted order of the array. If the target value is already present in the array, the bisect.bisect_right function returns the index of the element that is greater than the target.
+
+# After finding the insertion position of the target, we check if the element at the index idx-1 is equal to the target. If it is, we return the index idx-1 because it is the index of the first occurrence of the target in the array. If it isn't, we return -1 to indicate that the target value is not present in the array.
+
+# The time complexity of this solution is O(logN) because the bisect.bisect_right function uses binary search to find the insertion position of the target. The space complexity is O(1) because we only need a constant amount of extra space to store the idx variable.
