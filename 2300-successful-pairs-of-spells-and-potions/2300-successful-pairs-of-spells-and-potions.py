ba23eafdf31d9cc8f376ef1cb76725b85d8ae29b
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-04-07 14:52:56 +0530

    Time: 1183 ms (93.85%), Space: 37.2 MB (54.72%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -1,30 +1,27 @@
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
-        # Sorting + Two Pointers Approach
-        # 1. if spell = a ; minPotion = b, then spell * minPotion = success; For spell > a , then minimum required potion will be all minPotion <= b, if we need result >= success
-        # 2. If both spell and potions array are sorted in increasing order; two pointers - one to smallest spell; other to largest potion
-        # 3. If product of current spell and potion >= success, then we keep decreasing the second pointer to point to a smaller potion.
-        # 4. We stop if we dont form a successful pair, we count all previous approach.
+        # Approach - 1 : Sorting + Binary Search
         
-        sortedSpells = [(spell,index) for index,spell in enumerate(spells)]
-        
-        # Sort the ' spells with index' and 'potions' array in increasing order
-        sortedSpells.sort()  # O(n*logn)
-        print(sortedSpells)
-        potions.sort() # O(m * logm)
-        
-        answer = [0] * len(spells)
+        # Sort portions array in increasing order
+        potions.sort()
+        # 'answer' array to store result
+        answer = []
+        # length of potions array
         m = len(potions)
-        potionIndex = m - 1
+        # maximum potion in the potions array
+        maxPotion = potions[m-1]
         
-        # For each 'spell' find the respective 'minPotion' index
-        for spell, index in sortedSpells:   # O(n + m); we use two pointers and iterate on each element of the sortedSpells and potions array 
-            while potionIndex >= 0 and (spell * potions[potionIndex]) >= success:
-                # potionIndex is updated and doesn't change for each spell
-                potionIndex -= 1
-            answer[index] = m - (potionIndex + 1)
-        return answer
-    
-    # Time Complexity: O(n*logn) + O(m*logm) 
-    # Space Complexity: O(n + m), in python we use extra space to sort arrays in place
+        for spell in spells:
+            # minimum potion to make spell successful
+            minPotion = ceil(success/spell)
             
+            # Case when, if minPotion required is greater than the Max of potion array
+            if minPotion > maxPotion:
+                answer.append(0)
+                continue
+            # Binary Search
+            index = bisect.bisect_left(potions,minPotion)
+            answer.append(m-index)
+            
+        return answer
+        
