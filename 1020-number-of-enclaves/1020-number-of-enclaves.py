7fe866cef57bf0909fca01770f68623e60e6221b
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-04-07 11:11:58 +0530

    Time: 638 ms (83.22%), Space: 15.4 MB (98.72%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,23 +1,35 @@
+# Breadth First Search Approach
+import collections
+
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
-        ROWS, COLS  = len(grid), len(grid[0])
         
-        # Return num of land cells
-        def dfs(r,c):
-            if (r < 0 or c < 0 or r == ROWS or c == COLS or not grid[r][c] or (r,c) in visit):
-                return 0
-            visit.add((r,c))
-            res = 1
-            direct = [[0,1],[0,-1],[1,0],[-1,0]]
-            for dr,dc in direct:
-                res += dfs(r+dr, c+dc)
-            return res
+        q = collections.deque()
+        ROWS,COLS = len(grid),len(grid[0])
+        ans = 0
         
-        visit = set()
-        land, borderLand = 0,0
+        # Idea is to put only the boundary 1's into queue and eliminate all the neighbour 1's of those and itself, and then get the grid with the remaining 1's
+        
         for r in range(ROWS):
             for c in range(COLS):
-                land += grid[r][c]
-                if (grid[r][c] and (r,c) not in visit and (c in [0,COLS-1] or r in [0,ROWS-1])):
-                    borderLand += dfs(r,c)
-        return land - borderLand
+                if grid[r][c] == 1 and (r in [0,ROWS-1] or c in [0,COLS-1]):
+                    q.append((r,c))
+        dir = [[-1,0],[0,1],[1,0],[0,-1]]
+        
+        while q:
+            
+            r,c = q.popleft()
+            grid[r][c] = 0
+            for dr,dc in dir:
+                R = r + dr
+                C = c + dc
+                
+                if (R >= 0 and R < ROWS and C >= 0 and C < COLS and grid[R][C] == 1):
+                    grid[R][C] = 0
+                    q.append((R,C))
+        
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == 1:
+                    ans += 1
+        return ans
