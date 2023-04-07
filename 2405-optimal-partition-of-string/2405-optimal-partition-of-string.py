1147fcb5d9be9884408c8949b78ac76039aec20b
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-04-07 15:39:34 +0530

    Time: 241 ms (12.91%), Space: 14.6 MB (38.35%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -1,14 +1,29 @@
 class Solution:
     def partitionString(self, s: str) -> int:
         # Hash Set approach
-        e = ''
-        count = 0
-        for i in s:
-            if i in e:
-                count += 1
-                e = ' '
-                e += i
+#         e = ''
+#         count = 0
+#         for i in s:
+#             if i in e:
+#                 count += 1
+#                 e = ' '
+#                 e += i
                     
-            e += i
-        return count + 1
+#             e += i
+#         return count + 1
+    
+        # Greedy Approach 
+        lastSeen = [-1] * 26
+        count = 1
+        substringStarting = 0
+        
+        for i in range(len(s)):
+            if lastSeen[ord(s[i])-ord('a')] >= substringStarting:
+                count += 1
+                substringStarting = i
+    
+            lastSeen[ord(s[i])-ord('a')] = i
+        
+        return count
+    
         
