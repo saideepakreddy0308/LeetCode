f433ff3f156ea9d9e988887bc7371a8feef6f38b
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-04-14 18:26:13 +0530

    Added solution - LeetHub

--- Find minimum and maximum element in an array - GFG/find-minimum-and-maximum-element-in-an-array.py
+++ Find minimum and maximum element in an array - GFG/find-minimum-and-maximum-element-in-an-array.py
@@ -1,9 +1,14 @@
 #User function Template for python3
 
 def getMinMax( a, n):
-    a.sort()
-    return a[0],a[-1]
-    
+    min = a[0]
+    max = a[0]
+    for i in range(n):
+        if a[i] < min:
+            min = a[i]
+        elif a[i] > max:
+            max = a[i]
+    return min,max
     
 
 
