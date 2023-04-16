30e20c0358183280332023050cb17e64a143eb77
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-04-16 12:34:50 +0530

    Time: 27 ms (90.78%), Space: 13.8 MB (91.00%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,18 +1,20 @@
 class Solution:
     def fib(self, n: int) -> int:
         
-        # Top Down Approach; Memoization - Recursion
-        memo = {}
-        def fn(n,memo):
-            if n == 0 or n == 1:
-                return n
-            if n in memo:
-                return memo[n]
-            result = fn(n-1,memo) + fn(n-2,memo)
-            
-            memo[n] = result
-            return result
-        return fn(n,memo)
+        # Bottom up Approach; Tabulation - Iterative
+        # Creating array named dp
+        if n == 0:
+            return 0
+        
+        dp = [0] * (n+1)
+        # Base Cases
+        dp[0] = 0
+        dp[1] = 1
+        
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+    
+        return dp[n]
     
     # Time Complexity: O(N)
         # This is because each Fibonacci number from 0 to n is calculated only once and stored in the memoization table (dictionary) for future reference. Subsequent calculations for the same Fibonacci numbers are retrieved from the memoization table in constant time.
