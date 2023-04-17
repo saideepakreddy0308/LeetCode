378a8ab44889749a8ba60d600f83d07dbcddafb8
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-04-17 10:40:08 +0530

    Time: 42 ms (49.30%), Space: 14 MB (8.81%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -1,15 +1,19 @@
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         
-        # Brute Force Approach
-        high = max(candies)
+        # Brute Force Approach / Ad Hoc approach
+        # We first find out , which kid have greatest number of candies (high)
+        high = max(candies)  # O(N)
         n = len(candies)
         
-        answer = []
+        answer = []   
         
-        for i in range(n):
+        for i in range(n):   # O(N)
             if candies[i] + extraCandies < high:
                 answer.append(False)
             else:
                 answer.append(True)
         return answer
+    
+    # Time complexity: O(N)
+    # Space Complexity: O(1), The list we return is the auxiliary space and we never count auxiliary space when calculating space complexity
