b1e330d0eeff28a0b618c4d5343020faaf0eb41a
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-04-18 11:47:42 +0530

    Time: 24 ms (96.72%), Space: 13.9 MB (9.46%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,21 +1,18 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         
-        # Two Pointers Approach
+        # One Pointer Approach
         l1 = len(word1)
         l2 = len(word2)
         
-        i = 0
-        j = 0
+        n = max(l1,l2)
         
         result = []
         
-        while i < l1 or j < l2:
+        for i in range(n):
             if i < l1:
                 result += word1[i]
-                i += 1
-            if j < l2:
-                result += word2[j]
-                j += 1
-        
+            if i < l2:
+                result += word2[i]
+                
         return "".join(result)
