aa281e43abb40cc88dbfad45ea811c08e66f55b0
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-04-26 15:43:56 +0530

    Added solution - LeetHub

--- First and last occurrences of x - GFG/first-and-last-occurrences-of-x.py
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.py
@@ -3,23 +3,42 @@
 
 def find(arr,n,x):
     # code here
-    if n == 0:
-        return [-1,-1]
-    l = []
-    for i in range(n):
-        if arr[i] < x:
-            continue
-        elif arr[i] == x:
-            l.append(i)
-        elif arr[i] > x:
-            break
-    if len(l) == 0:
-        return [-1,-1]
-    return [l[0],l[-1]]
+    # Another approach - Binary Search in O(logN) t.c
     
-    # T.C: O(logN)
+    first_occurrence = -1
+    last_occurrence = -1
     
-
+    # given n,x
+    
+    # Find first occurence
+    low = 0
+    high = n - 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == x:
+            first_occurrence = mid
+            high = mid - 1
+        elif arr[mid] < x:
+            low = mid + 1
+        else:
+            high = mid - 1
+    
+    # Find last occurrence
+    low = 0
+    high = n - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == x:
+            last_occurrence = mid
+            low = mid + 1
+            
+        elif arr[mid] < x:
+            low = mid + 1
+        else:
+            high = mid - 1
+    
+    return first_occurrence, last_occurrence
 
 #{ 
  # Driver Code Starts
