6e67356206984a9d1d5df54426c5045c2e68b8b3
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-04-28 13:19:05 +0530

    Time: 2094 ms (23.93%), Space: 16.6 MB (10.16%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.py
+++ 0839-similar-string-groups/0839-similar-string-groups.py
@@ -1,31 +1,38 @@
 class Solution:
     def numSimilarGroups(self, strs: List[str]) -> int:
         n = len(strs)
-        ns = len(strs[0])
-        graph = [[] for i in range(n)]
-
-        for i, s1 in enumerate(strs):
-            for j, s2 in enumerate(strs):
-                count = 0
-                for c1, c2 in zip(s1, s2):
-                    if c1 != c2:
-                        count += 1
-                    if count > 2:
-                        break
-                else:
-                    graph[i].append(j)
-        
-        comp = 0
-        visited = [False] * n
-        def dfs(at):
-            if visited[at]:
-                return
-            visited[at] = True
-            for to in graph[at]:
-                dfs(to)
+        adj = self.createGraph(strs, n) # Create an adjacency list to represent the graph
+        vis = [False] * n # Create a boolean array to keep track of visited nodes
+        ans = 0
         for i in range(n):
-            if not visited[i]:
-                dfs(i)
-                comp += 1
-        
-        return comp
+            if not vis[i]: # Traverse the graph using BFS and count the number of connected components
+                self.bfs(i, vis, adj)
+                ans += 1
+        return ans
+
+    def bfs(self, i: int, vis: List[bool], adj: List[List[int]]) -> None: # Breadth-first search to traverse the graph
+        q = deque()
+        q.append(i)
+        vis[i] = True
+        while q:
+            t = q.popleft()
+            for j in adj[t]:
+                if not vis[j]:
+                    vis[j] = True
+                    q.append(j)
+
+    def createGraph(self, strs: List[str], n: int) -> List[List[int]]:
+        ans = [[] for _ in range(n)]
+        for i in range(n):
+            for j in range(i + 1, n):
+                if self.isSimilar(strs[i], strs[j]): # Check if two strings are similar with max of 1 swap
+                    ans[i].append(j)
+                    ans[j].append(i)
+        return ans
+
+    def isSimilar(self, a: str, b: str) -> bool:
+        c = 0
+        for i in range(len(a)):
+            if a[i] != b[i]:
+                c += 1
+        return c == 0 or c == 2
