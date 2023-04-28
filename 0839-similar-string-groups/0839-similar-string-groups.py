ab875d1419111f29cd4db38a02074da1dd400d86
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-04-28 13:15:33 +0530

    Time: 469 ms (56.72%), Space: 16.8 MB (10.16%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.py
+++ 0839-similar-string-groups/0839-similar-string-groups.py
@@ -1,23 +1,31 @@
 class Solution:
     def numSimilarGroups(self, strs: List[str]) -> int:
-        groups = 0
         n = len(strs)
-        vis = [False] * n
+        ns = len(strs[0])
+        graph = [[] for i in range(n)]
+
+        for i, s1 in enumerate(strs):
+            for j, s2 in enumerate(strs):
+                count = 0
+                for c1, c2 in zip(s1, s2):
+                    if c1 != c2:
+                        count += 1
+                    if count > 2:
+                        break
+                else:
+                    graph[i].append(j)
+        
+        comp = 0
+        visited = [False] * n
+        def dfs(at):
+            if visited[at]:
+                return
+            visited[at] = True
+            for to in graph[at]:
+                dfs(to)
         for i in range(n):
-            if vis[i]: continue
-            groups += 1
-            self.dfs(i, strs, vis)
-        return groups
-
-    def dfs(self, i: int, strs: List[str], vis: List[bool]) -> None:
-        vis[i] = True
-        for j in range(len(strs)):
-            if vis[j]: continue
-            if self.is_similar(strs[i], strs[j]):
-                self.dfs(j, strs, vis)
-
-    def is_similar(self, a: str, b: str) -> bool:
-        count = 0
-        for i in range(len(a)):
-            if a[i] != b[i]: count += 1
-        return count == 2 or count == 0
+            if not visited[i]:
+                dfs(i)
+                comp += 1
+        
+        return comp
