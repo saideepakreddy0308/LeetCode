bae06dc2c83dccaa2f656364414995dc80e7154d
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-06-05 14:32:21 +0530

    Added solution - LeetHub

--- Missing number in array - GFG/missing-number-in-array.py
+++ Missing number in array - GFG/missing-number-in-array.py
@@ -1,20 +1,20 @@
 #User function Template for python3
 
-# Binary Search Method
+
 class Solution:
-    def MissingNumber(self,array,n):
-        # code here
-        l = 0
-        r = n - 2
-        
-        array.sort()
-        while l <= r:
-            mid = l + (r-l)//2
-            if array[mid] == mid + 1:
-                l = mid + 1
-            else:
-                r = mid - 1
-        return l + 1
+    def missingNumber(self,array,n):
+       # code here
+       l = 0
+       r = n - 2
+       
+       array.sort()
+       while l <= r:
+           mid = l + (r-l)//2
+           if array[mid] == mid + 1:
+               l = mid + 1
+           else:
+               r = mid - 1
+       return l + 1
 
 #{ 
  # Driver Code Starts
@@ -27,6 +27,6 @@ t=int(input())
 for _ in range(0,t):
     n=int(input())
     a=list(map(int,input().split()))
-    s=Solution().MissingNumber(a,n)
+    s=Solution().missingNumber(a,n)
     print(s)
 # } Driver Code Ends
