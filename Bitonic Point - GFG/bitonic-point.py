e27cbcee5d5c994f5641e1df23e52c2e585b1c79
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-06-05 14:10:00 +0530

    Added solution - LeetHub

--- Bitonic Point - GFG/bitonic-point.py
+++ Bitonic Point - GFG/bitonic-point.py
@@ -2,10 +2,27 @@
 class Solution:
 
 	def findMaximum(self,arr, n):
-		# code here
-		return max(arr)  # O(logN)
-
-
+		l = 0
+        r = n-1
+        
+        while(l <= r):
+    
+            # finding mid by binary right shifting.
+            mid = (l + r ) >> 1
+    
+            # first case if mid is the answer
+            if((mid == 0 or arr[mid - 1] <= arr[mid]) and (mid == n - 1 or arr[mid + 1] <= arr[mid])):
+                break
+    
+            # move the right pointer
+            if(mid > 0 and arr[mid - 1] > arr[mid]):
+                r = mid - 1
+    
+            # move the left pointer
+            else:
+                l = mid + 1
+    
+        return arr[mid]
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
