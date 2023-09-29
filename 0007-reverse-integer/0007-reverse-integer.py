7136804fb93a57fa5d97ff3086665ffc2da16848
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-09-29 16:37:22 +0530

    Time: 38 ms (69.59%), Space: 16.2 MB (55.68%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x >= 0:
+            y = int(str(x)[::-1])
+            return y if y < 2147483648 else 0
+        else:
+            y = -(int(str(x)[:0:-1]))
+            return y if y > -2147483648 else 0
