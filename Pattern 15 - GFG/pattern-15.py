423bc633388018900bd816b1014ba418017cf1c9
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-09-29 22:51:00 +0530

    Added solution - LeetHub

---
+++ Pattern 15 - GFG/pattern-15.py
@@ -0,0 +1,22 @@
+#User function Template for python3
+
+class Solution:
+    def printTriangle(self, N):
+        # Code here
+        for i in range(65+N-1,64,-1):
+            for j in range(65,i+1):
+                print(chr(j),end="")
+            print('')
+        
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        N = int(input().strip())
+        ob = Solution()
+        ob.printTriangle(N)
+# } Driver Code Ends
