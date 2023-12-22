99d4eb326cc8e2a7223a24a674210f378079ef46
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-12-22 16:13:59 +0530

    Time: 75 ms (93.13%), Space: 17.3 MB (5.17%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        
+        dp = [0] * n
+        dp[0] = 1 # first ugly number to inert is 1.
+        p2,p3,p5 = 0,0,0 # for tracking positions of ugly number
+
+        for i in range(1, n):
+            ugly2, ugly3, ugly5 = dp[p2] * 2, dp[p3] * 3, dp[p5] * 5
+            dp[i] = min(ugly2, ugly3, ugly5)
+
+            if dp[i] == ugly2:
+                p2 += 1
+            if dp[i] == ugly3:
+                p3 += 1
+            if dp[i] == ugly5:
+                p5 += 1
+
+        return dp[-1]
