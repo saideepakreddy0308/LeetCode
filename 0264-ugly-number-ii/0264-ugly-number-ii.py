d9724ceb18a271d1d8907e380708230ef280c399
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-12-22 17:07:48 +0530

    Time: 505 ms (8.24%), Space: 17.5 MB (5.17%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -7,7 +7,11 @@ class Solution:
 
         for i in range(1, n):
             ugly2, ugly3, ugly5 = dp[p2] * 2, dp[p3] * 3, dp[p5] * 5
-            dp[i] = min(ugly2, ugly3, ugly5)
+            
+            dp[i] = min(ugly2,ugly3,ugly5)
+            
+            print(dp[i],'ugly2',ugly2,'ugly3',ugly3,'ugly5',ugly5)
+            # Observe for n = 10, we see ugly2 = 6, ugly3 = 6, at that time, we see the increment of both.
 
             if dp[i] == ugly2:
                 p2 += 1
@@ -15,5 +19,6 @@ class Solution:
                 p3 += 1
             if dp[i] == ugly5:
                 p5 += 1
-
+            
+            print('dp[i]:', dp[i], p2,p3,p5)
         return dp[-1]
