efc20486208c8db4ebff8ab5148374b47b1f03ab
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2023-12-26 15:01:38 +0530

    Time: 454 ms (79.13%), Space: 17.7 MB (58.27%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.py
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.py
@@ -1,17 +1,30 @@
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
-        
-        n = len(arr)
-        pq = []
-        
-        for i in range(n-1):
-            heapq.heappush(pq,( arr[i]/arr[-1],i,n-1)) 
-            
-        while k - 1:
-            _,i,j = heapq.heappop(pq)
-            if i < j - 1:
-                heapq.heappush(pq, (arr[i]/ arr[j-1],i,j-1))
-            
-            k -= 1
-        
-        return arr[pq[0][1]], arr[pq[0][2]]
+        lo, hi = 0, 1
+
+        while True:
+            mid = (lo + hi) / 2
+            j = 0
+            count = 0
+            maxFrac = [0, 1]
+
+            for i in range(len(arr)):
+                while j < len(arr) and arr[i] > mid * arr[j]:
+                    print(" mid is",mid," j is ", j," ", arr[i], arr[j], mid * arr[j] )
+
+                    j += 1
+                    print(" j is incremented", j)
+
+                count += len(arr) - j
+                print("count is", count)
+
+                if j < len(arr) and maxFrac[0] * arr[j] < arr[i] * maxFrac[1]:
+                    maxFrac = [arr[i], arr[j]]
+                    print("maxFrac is", maxFrac, maxFrac[0] * arr[j], arr[i] * maxFrac[1] )
+                    
+            if count == k:
+                return maxFrac
+            elif count > k:
+                hi = mid
+            else:
+                lo = mid
