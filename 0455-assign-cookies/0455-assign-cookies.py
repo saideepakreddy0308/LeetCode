7a7cbf80904c9691ddc51b2e7ff22c422a607192
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2024-01-03 00:52:40 +0530

    Time: 228 ms (11.82%), Space: 19.2 MB (14.80%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -1,13 +1,17 @@
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
-        n=len(g)-1
-        m=len(s)-1
-        g=sorted(g)
-        s=sorted(s)
-        ans=0
-        while min(n,m)>=0:
-            if g[n]<=s[m]:
-                ans+=1
-                m-=1
-            n-=1
-        return ans  
+        
+        
+        # Greedy approach trying to give children smaller cookies earlier based on their content, so as to keep larger cookies for further. [Greedy]
+        g.sort()  # content needed
+        s.sort()  # available cookie sizes
+        
+        content_children = 0
+        cookie_index = 0
+        
+        while cookie_index < len(s) and content_children < len(g):
+            if s[cookie_index] >= g[content_children]:
+                content_children += 1
+            cookie_index += 1
+        
+        return content_children
