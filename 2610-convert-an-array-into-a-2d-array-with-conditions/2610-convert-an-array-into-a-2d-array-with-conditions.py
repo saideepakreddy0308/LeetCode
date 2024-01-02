6fda03dda20c9b60d40001521c1bb1ed80961fb4
Deepak <90776053+saideepakreddy0308@users.noreply.github.com>
2024-01-03 00:24:36 +0530

    Time: 99 ms (5.01%), Space: 17.5 MB (5.17%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
@@ -1,22 +1,21 @@
-from collections import Counter
-from typing import List
-
 class Solution:
     def findMatrix(self, nums: List[int]) -> List[List[int]]:
-        ht = Counter(nums)
-        result = []
+        memo = defaultdict(int)
+        for num in nums:
+            memo[num] += 1
 
-        x = max(ht.values())
+        heap = []
+        for key in memo:
+            heappush(heap, (-memo[key], key))
+        
+        freq, num = heappop(heap)
+        freq = -freq
 
-        for _ in range(x):
-            l = []
-            for key, value in ht.items():
-                if value >= 1:
-                    l.append(key)
-                    ht[key] -= 1
-            result.append(l)
+        res = [[num] for  i in range(freq)]
 
-        return result
-    
-# t.c: O(x * n), x is max.val of a single no, and n is no.of unique elts in ht
-# s.c: O(n)
+        while heap:
+            freq, num = heappop(heap)
+            for i in range(-freq):
+                res[i].append(num)
+            
+        return res
